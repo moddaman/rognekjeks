a9b28e471ba7bdaf639d2e6b071a4e3262464600
moddaman <ovrebo90@gmail.com>
2019-02-09 19:38:34 +0100

    Updates

---
+++ precache-manifest.83d7b63659a9ed6b836e35f323e1670f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/rognekjeks/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "4b7577ca7da701465623",
+    "url": "/rognekjeks/static/js/runtime~main.4b7577ca.js"
+  },
+  {
+    "revision": "ecf9bc9f9f0ae774e79e",
+    "url": "/rognekjeks/static/js/main.ecf9bc9f.chunk.js"
+  },
+  {
+    "revision": "13eeb2032981bf3107f9",
+    "url": "/rognekjeks/static/js/1.13eeb203.chunk.js"
+  },
+  {
+    "revision": "ecf9bc9f9f0ae774e79e",
+    "url": "/rognekjeks/static/css/main.3113a640.chunk.css"
+  },
+  {
+    "revision": "f1247e880dd1d51fb867587d3dfbcb77",
+    "url": "/rognekjeks/index.html"
+  }
+];
